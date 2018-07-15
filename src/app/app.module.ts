ab8549f6595268fa67d2e3892b2855c6b2effdca
unknown <trollgon@gmail.com>
2018-07-15 09:49:16 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WeatherComponent} from './components/weather/weather.component';
+import {BestWeatherComponent} from './components/best-weather/best-weather.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ForecastComponent} from './components/forecast/forecast.component';
+
+import {appRouting} from './app.routing';
+
+import {WeatherService} from './services/weather.service';
+import {ResolveLocationService} from './services/resolve-location.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    BestWeatherComponent,
+    HeaderComponent,
+    ForecastComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    appRouting,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [WeatherService, ResolveLocationService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
